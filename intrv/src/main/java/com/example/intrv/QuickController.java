6fed3cce24693344e296bfd413c16e6df5301f35
ltchami <ltchami@techtarget.com>
2023-01-07 14:41:43 -0700

    Interview Metrostar

---
+++ intrv/src/main/java/com/example/intrv/QuickController.java
@@ -0,0 +1,16 @@
+package com.example.intrv;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class QuickController {
+	
+	@CrossOrigin
+	@GetMapping("/")
+	public String index() {
+		return "Hello World";
+	}
+
+}
